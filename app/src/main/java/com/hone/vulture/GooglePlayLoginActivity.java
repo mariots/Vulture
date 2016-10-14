46c7ffa4504d44afa13a5b7251e43f0e84e1b5f6
alexweav <alexander.weaver@ttu.edu>
2016-10-14 16:17:27 -0500

    Beginning sign-in functionality

--- app/src/main/java/com/hone/vulture/GooglePlayLoginActivity.java
+++ app/src/main/java/com/hone/vulture/GooglePlayLoginActivity.java
@@ -1,20 +1,36 @@
 package com.hone.vulture;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
+
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.games.Games;
 
 
-public class GooglePlayLoginActivity extends Activity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class GooglePlayLoginActivity extends FragmentActivity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleApiClient mGoogleApiClient;
 
     private boolean mSignInClicked = false;
+    private boolean mIsInResolution;
+
+    private static final String TAG = "GooglePlayServicesActiv";
+
+    /**
+     * Request code for auto Google Play Services error resolution.
+     */
+    protected static final int REQUEST_CODE_RESOLUTION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +46,12 @@ public class GooglePlayLoginActivity extends Activity implements View.OnClickLis
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
     public void onConnected(Bundle connectionHint) {
         ((TextView)findViewById(R.id.status_text)).setText("Connection succeeded!");
         findViewById(R.id.sign_in_button).setVisibility(View.GONE);
@@ -37,8 +59,33 @@ public class GooglePlayLoginActivity extends Activity implements View.OnClickLis
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-
-        ((TextView)findViewById(R.id.status_text)).setText("Connection failed: " + connectionResult.getErrorMessage());
+        //((TextView)findViewById(R.id.status_text)).setText("Connection failed: " + connectionResult.getErrorMessage());
+        Log.i(TAG, "GoogleApiClient connection failed: " + connectionResult.toString());
+        if (!connectionResult.hasResolution()) {
+            // Show a localized error dialog.
+            GooglePlayServicesUtil.getErrorDialog(
+                    connectionResult.getErrorCode(), this, 0, new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            retryConnecting();
+                        }
+                    }).show();
+            return;
+        }
+        // If there is an existing resolution error being displayed or a resolution
+        // activity has started before, do nothing and wait for resolution
+        // progress to be completed.
+        if (mIsInResolution) {
+            return;
+        }
+        mIsInResolution = true;
+        try {
+            connectionResult.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+            mGoogleApiClient.connect();
+        } catch (IntentSender.SendIntentException e) {
+            Log.e(TAG, "Exception while starting resolution activity", e);
+            retryConnecting();
+        }
     }
 
     @Override
@@ -53,9 +100,46 @@ public class GooglePlayLoginActivity extends Activity implements View.OnClickLis
             text.setText("Signing into Google Play...");
             mSignInClicked = true;
             mGoogleApiClient.connect();
+            //((TextView)findViewById(R.id.status_text)).setText("Google Play services availability: " + isGooglePlayServicesAvailable(this));
         }
         if (view.getId() == R.id.offline_button) {
             //Proceed to main menu activity here
         }
+    }
+
+    private void retryConnecting() {
+        mIsInResolution = false;
+        if (!mGoogleApiClient.isConnecting()) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+        if (requestCode == 9001) {
+            mSignInClicked = false;
+            if (resultCode == RESULT_OK) {
+                mGoogleApiClient.connect(GoogleApiClient.SIGN_IN_MODE_REQUIRED);
+            } else {
+                // Bring up an error dialog to alert the user that sign-in
+                // failed. The R.string.signin_failure should reference an error
+                // string in your strings.xml file that tells the user they
+                // could not be signed in, such as "Unable to sign in."
+                ((TextView)findViewById(R.id.status_text)).setText("9001");
+            }
+        }
+    }
+
+
+    public boolean isGooglePlayServicesAvailable(Activity activity) {
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int status = googleApiAvailability.isGooglePlayServicesAvailable(activity);
+        if(status != ConnectionResult.SUCCESS) {
+            if(googleApiAvailability.isUserResolvableError(status)) {
+                googleApiAvailability.getErrorDialog(activity, status, 2404).show();
+            }
+            return false;
+        }
+        return true;
     }
 }
