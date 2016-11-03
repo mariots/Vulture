4df1ac3d8f67b5ba8123558e555dbd0f039abd5e
alexweav <alexander.weaver@ttu.edu>
2016-11-03 08:20:55 -0500

    Added GoogleApiHelper

---
+++ app/src/main/java/com/hone/vulture/GoogleApiHelper.java
@@ -0,0 +1,67 @@
+package com.hone.vulture;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+
+/**
+ * Created by alexweav on 11/2/16.
+ */
+
+public class GoogleApiHelper implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleApiClient mGoogleApiClient;
+
+    public GoogleApiHelper(Context context) {
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+        connect();
+    }
+
+    public GoogleApiClient getGoogleApiClient() {
+        return this.mGoogleApiClient;
+    }
+
+    public void connect() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    public void disconnect() {
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    public boolean isConnected() {
+        if (mGoogleApiClient != null) {
+            return mGoogleApiClient.isConnected();
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+
+    }
+}
