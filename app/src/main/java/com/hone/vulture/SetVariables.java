ed57a283aa01fc6891c6423005c2a10066f84e64
mariot <mario@teamhone.io>
2016-11-12 21:20:31 -0600

    Added Set Variables

---
+++ app/src/main/java/com/hone/vulture/SetVariables.java
@@ -0,0 +1,13 @@
+package com.hone.vulture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetVariables extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_variables);
+    }
+}
