e862f9c9a1221032735c4dc51429047225bcce0f
alexweav <alexander.weaver@ttu.edu>
2016-11-16 23:32:46 -0600

    Button functionality in lobby

--- app/src/main/java/com/hone/vulture/GameLobbyActivity.java
+++ app/src/main/java/com/hone/vulture/GameLobbyActivity.java
@@ -1,30 +1,33 @@
 package com.hone.vulture;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class GameLobbyActivity extends AppCompatActivity {
+public class GameLobbyActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_lobby);
-        View.OnClickListener lister = new View.OnClickListener() {
+        findViewById(R.id.AddFriendButton).setOnClickListener(this);
+        findViewById(R.id.ReadyButton).setOnClickListener(this);
+        findViewById(R.id.QuitButton).setOnClickListener(this);
+    }
 
-            public void onClick(View view) {
-                if (view.getId() == R.id.AddFriendButton) {
-
-                }
-                if (view.getId() == R.id.ReadyButton) {
-                    /*Intent intent = new Intent(GameLobby.this, Game.class);//Intent to Start a Game
-                    startActivity(intent);*/
-                    ;
-                }
-                if (view.getId() == R.id.QuitButton) {
-                    /*Intent intent = new Intent(GameLobby.this, MainMenuActivity.class);//Intent to Go back to Main Menu
-                    startActivity(intent);*/
-                }
-            }
-        };
-    }}
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.ReadyButton) {
+            Intent intent = new Intent(this, InGameActivity.class);//Intent to Start a Game
+            startActivity(intent);
+        }
+        if (view.getId() == R.id.QuitButton) {
+            Intent intent = new Intent(this, MainMenuActivity.class);//Intent to Start a Game
+            startActivity(intent);
+        }
+        if (view.getId() == R.id.AddFriendButton) {
+            //nothing yet
+        }
+    }
+}
